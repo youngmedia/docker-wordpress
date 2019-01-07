25aa621669327407fb58bdcdebe2c2c6447ff293
Administrator <admin@example.com>
2019-01-07 16:49:35 +0000

    Develop

--- src/advanced-cache.php
+++ src/advanced-cache.php
@@ -2,8 +2,8 @@
 // Created by Speedmaster
 defined( 'ABSPATH' ) or die( 'Cheatin\' uh?' );
 
-define( 'SPEEDMASTER_ACTIVATED', true );
+define( 'SPEEDMASTER__AC_FILE_CHECK', 1 );
 
-if (file_exists('/var/www/html/wp-content/plugins/speedmaster/boot.php')) {
-  require_once( '/var/www/html/wp-content/plugins/speedmaster/boot.php');
+if (file_exists('/var/www/html/wp-content/plugins/speedmaster-redis/boot.php')) {
+  require_once( '/var/www/html/wp-content/plugins/speedmaster-redis/boot.php');
 }
