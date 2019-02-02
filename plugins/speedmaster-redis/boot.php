af93f1b1494ce190bcd945238510f9d046f64a78
Rasmus <rk@youngmedia.se>
2019-02-02 11:54:01 +0100

    Update boot.php

--- plugins/speedmaster-redis/boot.php
+++ plugins/speedmaster-redis/boot.php
@@ -1,4 +1,12 @@
 <?php
+namespace Speedmaster;
+
+define( 'SM__TIMESTAMP_BOOT', microtime(true));
+
+// Load vendor libraries
+require_once('lib/credis/Client.php');
+require_once('lib/credis/Cluster.php');
+
 // Load Speedmaster helpers
 require_once('helpers/Admin.php');
 require_once('helpers/CacheStorage.php');
